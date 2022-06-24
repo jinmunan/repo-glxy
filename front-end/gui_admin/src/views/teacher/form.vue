8f76174ecf62b51c939b063b5e475593e182914b
jinmunan <zn923042857@gmail.com>
2022-06-24 09:00:09 +0800

    谷粒学院

--- front-end/gui_admin/src/views/teacher/form.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="app-container">
-    <h1>讲师管理</h1>
-    <p>讲师表单</p>
-  </div>
-</template>
--- front-end/gui_admin/src/views/teacher/list.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="app-container">
-    <h1>讲师管理</h1>
-    <p>讲师列表</p>
-  </div>
-</template>
